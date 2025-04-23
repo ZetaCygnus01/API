e41fe287bf345b5a87b3453f97bfef1d11e5e1ed
ZetaCygnus01 <122745586+ZetaCygnus01@users.noreply.github.com>
2025-04-23 22:16:25 +0000

    Final2_API

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-//import { AppProvider } from './contexto/contexto';//
+import { AppProvider } from './contexto/contexto';
 
 import Menu from './componentes/Menu'
 import Aleatorios from './componentes/Aleatorios'
 import Listar from './componentes/Listar'
-
+import Capturados from './componentes/capturados';
 import Favoritos from './componentes/Favoritos'
 import Usuario from './componentes/Usuario'
 import Detalle from './componentes/Detalle'
@@ -19,14 +19,14 @@ function App() {
       <Menu/>
 
       <Routes>
-        
       <Route path="/" element={<Listar/>}/>
       <Route path="/Usuario" element={<Usuario/>}/>
       <Route path="/Aleatorios" element={<Aleatorios/>}/>
-      //capturados//
+      <Route path="/capturados" element={<Capturados />} />
       <Route path="/Favoritos" element={<Favoritos/>}/>
-      <Route path="/Detalle" element={<Detalle/>}/>
+      <Route path="/Detalle/:name" element={<Detalle />} />
       </Routes>
+
     </Router>
     </AppProvider>
   )
