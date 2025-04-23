393833d317455951e5f8648ba4db738680fbab31
ZetaCygnus01 <122745586+ZetaCygnus01@users.noreply.github.com>
2025-04-23 22:28:28 +0000

    ArregloLista

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { AppProvider } from './contexto/contexto';
 
 import Menu from './componentes/Menu'
 import Aleatorios from './componentes/Aleatorios'
-import Listar from './componentes/Listar'
+import Lista from './componentes/Lista'
 import Capturados from './componentes/capturados';
 import Favoritos from './componentes/Favoritos'
 import Usuario from './componentes/Usuario'
@@ -19,11 +19,10 @@ function App() {
       <Menu/>
 
       <Routes>
-      <Route path="/" element={<Listar/>}/>
+      <Route path="/" element={<Lista/>}/>
       <Route path="/Usuario" element={<Usuario/>}/>
       <Route path="/Aleatorios" element={<Aleatorios/>}/>
       <Route path="/capturados" element={<Capturados />} />
-      <Route path="/Favoritos" element={<Favoritos/>}/>
       <Route path="/Detalle/:name" element={<Detalle />} />
       </Routes>
 
